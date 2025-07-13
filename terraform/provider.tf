1682c7b1e256bc994d0f871b1e3e7fdd99fe82d5
Udayy Bambal <62509938+udaybambal@users.noreply.github.com>
2025-07-13 21:55:23 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.67.0"  # Pinned to work with your modules
+    }
+  }
+}
+
 locals {
 
   region          = "eu-west-1"
